f4febcc71ef1965b62f568866c94482514d4818d
陈世锋 <shifeng.chen@hand-china.com>
2019-01-07 01:53:59 +0800

    模板

---
+++ java-common/src/main/java/com/csf/common/throttle/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by hand on 2019/1/7.
+ */
+package com.csf.common.throttle;
---
+++ java-common/src/main/java/com/csf/common/throttle/ratelimit/RateLimit.java
@@ -0,0 +1,7 @@
+package com.csf.common.throttle.ratelimit;
+
+/**
+ * Created by hand on 2019/1/7.
+ */
+public class RateLimit {
+}
